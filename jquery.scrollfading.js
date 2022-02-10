25116eb1e3faa116dd3828e459c1747b25d2c9b1
Judzhin Miles <info@msbios.com>
2022-02-10 22:53:12 +0200

    Add Style

--- jquery.scrollfading.js
+++ jquery.scrollfading.js
@@ -4,28 +4,28 @@
  */
 $.fn.scrollfading = function (options) {
 
-    this.fadeTo(0, 0); // reset
-
-    var defaults = $.extend({
-        speed: 300
-    }, options);
-
-    var $w = $(window), $d = $(document), scrollfading = $.proxy(function (speed) {
-        this.each(function (i, e) {
-            var $e = $(e), a = $e.offset().top + $e.height(), b = $w.scrollTop() + $w.height();
-            if (a < b) {
-                $e.fadeTo(speed, 1);
-            }
-        });
-    }, this)
-
-    $w.load(function (e) {
-        scrollfading(defaults.speed);
-    });
-
-    $w.scroll(function (e) {
-        if ($w.scrollTop() + $w.height() <= $d.height()) {
-            scrollfading(defaults.speed * 2);
-        }
-    });
+    // this.fadeTo(0, 0); // reset
+    //
+    // var defaults = $.extend({
+    //     speed: 300
+    // }, options);
+    //
+    // var $w = $(window), $d = $(document), scrollfading = $.proxy(function (speed) {
+    //     this.each(function (i, e) {
+    //         var $e = $(e), a = $e.offset().top + $e.height(), b = $w.scrollTop() + $w.height();
+    //         if (a < b) {
+    //             $e.fadeTo(speed, 1);
+    //         }
+    //     });
+    // }, this)
+    //
+    // $w.load(function (e) {
+    //     scrollfading(defaults.speed);
+    // });
+    //
+    // $w.scroll(function (e) {
+    //     if ($w.scrollTop() + $w.height() <= $d.height()) {
+    //         scrollfading(defaults.speed * 2);
+    //     }
+    // });
 }
